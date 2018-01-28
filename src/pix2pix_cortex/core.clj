31769aa0206eda56eaca689b9c70122eb5e37fe2
Kieran Browne <kierantbrowne@gmail.com>
2018-01-29 00:08:46 +1100

    tweaks

--- src/pix2pix_cortex/core.clj
+++ src/pix2pix_cortex/core.clj
@@ -16,14 +16,19 @@
      :result (clisk/vplasma (clisk/v* 0.1 'i))
      :bailout-result clisk/black
      :max-iterations 1000
-     )) :width 400 :height 400))
+     )) :width 256 :height 256))
 
 
 (img/show
  (mandelbrot -1.31 0 00.1))
 
 (img/show
- (mandelbrot -0.74603 0.11002 0.00000001001))
+ (mandelbrot -0.74603 0.11002 0.0009))
+(map mikera.image.colours/values-rgb
+ (img/get-pixels
+  (mandelbrot -0.74603 0.11002 0.0009)))
+
+(take 100 (iterate (partial * 0.9) 0.1))
 
 (defn img-data [n]
   (map mikera.image.colours/values-rgb
@@ -31,31 +36,20 @@
         (clisk/image [clisk/x clisk/y 0] :width 2 :height 2)))
   )
 
+(hash-map :x 1 :y 2)
+
 (def as-normals (comp (partial map mikera.image.colours/values-rgb) img/get-pixels))
 
-(img/show
- (clisk/image
-  (clisk/fractal
-   :update (clisk/v+ clisk/x [clisk/x clisk/y])
-   :bailout-result clisk/black
-   :max-iterations clisk/black
-   )
-  :width 200 :height 200))
 
-
-(clisk/show
- (clisk/viewport
-  [-2 -1.5] [1 1.5]
-  (clisk/fractal
-   :while (clisk/v- 2 (clisk/length [clisk/x clisk/y]))
-   :update (clisk/v+ clisk/c [(clisk/v- (clisk/v* clisk/x clisk/x) (clisk/v* clisk/y clisk/y)) (clisk/v* 2 clisk/x clisk/y)])
-   :result (clisk/vplasma (clisk/v* 0.1 'i))
-   :bailout-result clisk/black
-   :max-interations 1000
-   )) 512 512)
 
 (def training-set
-  [{:x [1] :y [2]}])
+  (let [mandelbrot (comp as-normals (partial mandelbrot -0.74603 0.11002))]
+    (map #(hash-map :x (mandelbrot %) :y (mandelbrot (* % 0.9)))
+         (iterate (partial * 0.9) 0.01))))
+
+
+(first training-set)
+
 (layers/input 256 256 3 :id :x)
 
 (def encoder-decoder
@@ -63,6 +57,7 @@
     [(layers/input 256 256 3 :id :x)
      (layers/convolutional {:kernel-dim 4 :pad 2 :stride 1 :num-kernels 64})
      (layers/batch-normalization :epsilon 1e-5)
+     (layers/convolutional {:kernel-dim 4 :pad 2 :stride 1 :num-kernels 64})
 
      (layers/linear->tanh 10)]))
 
